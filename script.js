bb32ca11c38f1b9fa96bc004c503c15ccc29875d
ASHISHDWIVEDI-INFO <ashishdwivedi2410@gmail.com>
2026-05-28 11:34:56 +0000

    Your commit message here

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 function showMessage() {
-<<<<<<< HEAD
-    alert("Email: ashish@example.com");
+    alert("Email: ashishdwivedi2410@gmail.com");
 }
-=======
-    alert("Email: ashish@gmail.com");
-}
->>>>>>> 2c45e03 (Your commit message here)
